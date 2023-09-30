920466b215635e0b44a184799aa4f38ae87d721d
JorisDM <joris.demeyer@student.hogent.be>
2023-09-30 16:15:30 +0200

    europond

---
+++ Oefening D01euronaarpound/Oefening D01euronaarpound/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Oefening_D01euronaarpound
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double euroBedrag = 105.4;
+
+            // bekijk of je nog variabelen ontbreekt (nog variabelen moet declareren)
+            
+            double poundBedragWissel = euroBedrag * 0.88 ;
+            string poundBedrag = poundBedragWissel.ToString("F3");
+
+            // ken de juiste waardes toe aan alle variabelen,
+            // eventueel gebaseerd op de juist formule
+
+            Console.Write(euroBedrag);
+            Console.Write("EUR = ");
+            Console.Write(poundBedrag);
+            Console.Write("GPB");
+        }
+    }
+}
