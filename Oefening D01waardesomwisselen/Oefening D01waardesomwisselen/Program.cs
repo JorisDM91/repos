8f9f7b920eae2be8706984d38067d82e31a07fee
JorisDM <joris.demeyer@student.hogent.be>
2023-09-30 14:52:13 +0200

    WaardeOmwisselen

---
+++ Oefening D01waardesomwisselen/Oefening D01waardesomwisselen/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Oefening_D01waardesomwisselen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 13;
+
+            // 	verwissel hier de inhoud van de variabelen a en b
+
+            int c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+        }
+    }
+}
